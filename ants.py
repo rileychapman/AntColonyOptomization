86acbef350252067bc7609a09d6da3ee57ab8f46
Riley Chapman <rlychpmn@gmail.com>
2014-10-21 22:39:13 -0400

    working prompt

--- ants.py
+++ ants.py
@@ -24,9 +24,11 @@ class Ant(object):
         """
         numerators = []
         notVisitedPoints = []
+        possiblePoints = []
         for i in range(len(self.points)):
             if self.points[i] not in visitedPoints:
                 possiblePoint = self.points[i]
+                possiblePoints.append(possiblePoint)
                 lastPoint = visitedPoints[-1]
                 distance = sqrt((possiblePoint[0] - lastPoint[0])**2 + (possiblePoint[1] - lastPoint[1])**2)
                 indexI = self.points.index(possiblePoint)
@@ -39,6 +41,9 @@ class Ant(object):
         probabilities = []
         for numerator in numerators:
             probabilities.append(int(100*numerator/denominator))
+        #print "Location: "+ str(visitedPoints[-1])
+        #print "Points: "+ str(possiblePoints)
+        #print "Probabilities: "+ str(probabilities)
         weightedIndexes = []
         for i in range(len(notVisitedPoints)):
             index = self.points.index(notVisitedPoints[i])
@@ -65,32 +70,44 @@ class AntWorld(object):
         self.beta = beta
         self.evaporation = evaporation
         self.pheromones = np.zeros([len(self.points), len(self.points)])
+        self.pheromones.fill(1)
         np.fill_diagonal(self.pheromones, -1)
         self.ants = [Ant(points, alpha, beta) for a in range(ants)]
+        self.bestPath = []
+        self.bestPathDistance = -1
     
     def updatePheromones(self,tours):
         """
         update pheromone levels in world
         """
+
+        for i in range(len(self.points)):
+            for j in range(i):
+                self.pheromones[i][j] = self.evaporation * self.pheromones[i][j]
+                self.pheromones[j][i] = self.evaporation * self.pheromones[j][i]
+
         for tour in tours:
             distance = 0
-            '''not sure about the -1... might be -2?'''
             for i in range(len(tour)-1):
                 distance += sqrt(
                         (tour[i][0]-tour[i+1][0])**2 
                         + (tour[i][1]-tour[i+1][1])**2
                         )
+            
+            if self.bestPathDistance == -1:
+                self.bestPathDistance  = distance
+                self.bestPath = tour
+            elif distance < self.bestPathDistance:
+                self.bestPath = tour
+                self.bestPathDistance = distance
+
             for i in range(len(tour)-1):
-                self.pheromones[self.points.index(tour[i])][self.points.index(tour[i+1])] = (
-                        1/distance 
-                        + self.evaporation
-                        * self.pheromones[self.points.index(tour[i])][self.points.index(tour[i+1])]
-                        )
-                self.pheromones[self.points.index(tour[i+1])][self.points.index(tour[i])] = (
-                        1/distance 
-                        + self.evaporation
-                        * self.pheromones[self.points.index(tour[i+1])][self.points.index(tour[i])]
-                        )
+                addition = (1/distance**2)*50
+                currentLevel = self.pheromones[self.points.index(tour[i])][self.points.index(tour[i+1])]
+                print "Current: "+ str(currentLevel)+ " + "+str(addition)+" = "+str(addition + currentLevel)
+                self.pheromones[self.points.index(tour[i])][self.points.index(tour[i+1])] +=  addition 
+                self.pheromones[self.points.index(tour[i+1])][self.points.index(tour[i])] += addition 
+
         
     def antTours(self):
         tourPoints = []
@@ -103,6 +120,7 @@ class AntWorld(object):
             self.antTours()
 
     def draw(self):
+        print self.pheromones
         G = nx.Graph()
         edge_colors = list()
         min_pher = np.ma.masked_equal(self.pheromones, -1, copy=False).min()
@@ -111,7 +129,10 @@ class AntWorld(object):
             G.add_node(i, posxy=(points[i]))
             for j in range(i):
                 G.add_edge(i, j)
-                edge_color = tuple((1-(self.pheromones[i][j])/(max_pher-min_pher) for c in range(len('rgb'))))
+                color = self.pheromones[i][j]
+                if color > 1:
+                    color = 1
+                edge_color = tuple([0,0,color])
                 edge_colors.append(edge_color)
         pos = nx.get_node_attributes(G, 'posxy')
         nx.draw(
@@ -126,11 +147,14 @@ class AntWorld(object):
 if __name__ == "__main__":
 	
     alpha = 1 
-    beta = 1
-    evaporation = .75
+    beta = 3
+    evaporation = .8
     points = [[0., 0.], [3.,0.], [4., 2.], [3, 3.], [2., 2.]]
     ants = 1
-    generations = 2
     antworld = AntWorld(points, alpha, beta, ants, evaporation)
-    antworld.generateTours(generations)
-    antworld.draw()
+    while True:
+        generations = input('run generations: ')
+        antworld.generateTours(generations)
+        print "Best Path: "+str(antworld.bestPathDistance)+" "+str(antworld.bestPath)
+        print antworld.pheromones
+        #antworld.draw()
